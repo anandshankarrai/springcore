17fafdf8d6538b886008405487f92a9f32aa5f27
anandshankarrai <rudra.shankar0001@gmail.com>
2022-11-02 22:09:33 +0530

    Hello World again and again and again and again

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -17,6 +17,7 @@ public class App
 		System.out.println("Hello World again");
 		System.out.println("Hello World again and again");
 		System.out.println("Hello World again and again and again");
+		System.out.println("Hello World again and again and again and again");
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring/config.xml");
 		Student student =  (Student) context.getBean("student1");
